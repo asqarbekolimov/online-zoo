ef08901a9aa13e385cb0cd36441d32b43b8496a9
asqarbekolimov <mr.coderx.pro@gmail.com>
2026-03-19 14:12:12 +0500

    feat: add styles for map and zoo sections

--- online-zoo/app/layout.tsx
+++ online-zoo/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Montserrat, Montserrat_Alternates } from "next/font/google";
+import "./styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-primary",
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserratAlternates = Montserrat_Alternates({
+  variable: "--font-decorative",
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} ${montserratAlternates.variable} antialiased`}
       >
         {children}
       </body>
