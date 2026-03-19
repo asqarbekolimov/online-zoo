3cbff9f0f78d4fb303cb71b4456307187bf768a3
asqarbekolimov <mr.coderx.pro@gmail.com>
2026-03-19 11:13:45 +0500

    feat: add ESLint and Prettier configuration with husky and lint-staged setup

--- online-zoo/eslint.config.mjs
+++ online-zoo/eslint.config.mjs
@@ -1,18 +1,20 @@
+// @ts-check
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  prettier,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
