ecc1cd38efa10b19c37b95f3d1324ec23da042a1
asqarbekolimov <mr.coderx.pro@gmail.com>
2026-03-21 11:32:48 +0500

    feat: add layout and map pages with components integration

--- online-zoo/app/(marketing)/layout.tsx
+++ online-zoo/app/(marketing)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Montserrat_Alternates } from "next/font/google";
-import "./styles/globals.css";
+import "@/styles/globals.css";
 import { Footer, Navbar } from "@/components/layout";
 
 const montserrat = Montserrat({
---
+++ online-zoo/app/(marketing)/map/page.tsx
@@ -0,0 +1,15 @@
+const MapPage = () => {
+  return (
+    <main>
+      <section className="map">
+        <div className="container">
+          <div className="map-text">
+            <h2 className="heading-2">find where are the animals live</h2>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default MapPage;
